c4d99b6bff7a5cdc04cc7849245a2b0a4ffe5879
Jessie <2302541+Kangaroux@users.noreply.github.com>
2024-05-23 16:48:45 -0400

    Clarify test data endianness

--- test_data/calculate_x.py
+++ test_data/calculate_x.py
@@ -9,7 +9,7 @@ for _ in range(100):
     row = [
         random_string("ascii", 16).upper(), # username
         random_string("ascii", 16).upper(), # password
-        random_string("hex", 64).upper(), # salt (32 bytes)
-        "EXPECTED_VALUE_HERE",
+        random_string("hex", 64).upper(), # salt (32 bytes, little endian)
+        "REPLACE_ME_IN_CSV", # expected value (20 bytes, little endian)
     ]
     print(",".join(row))
