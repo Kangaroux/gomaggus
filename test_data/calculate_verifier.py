4fdb9baf4b54352747d0f1eb718be383387f6031
Jessie <2302541+Kangaroux@users.noreply.github.com>
2024-05-23 16:52:33 -0400

    Add test data for verifier

---
+++ test_data/calculate_verifier.py
@@ -0,0 +1,15 @@
+"""
+Usage:
+$ python3 calculate_verifier.py > calculate_verifier.csv
+"""
+
+from gen import random_string
+
+for _ in range(100):
+    row = [
+        random_string("ascii", 16).upper(), # username
+        random_string("ascii", 16).upper(), # password
+        random_string("hex", 64).upper(), # salt (32 bytes, little endian)
+        "REPLACE_ME_IN_CSV", # expected value (20 bytes, little endian)
+    ]
+    print(",".join(row))
