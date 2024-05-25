07214ae5598179f4e70cb9bed5df0cda5d7bef08
Jessie <2302541+Kangaroux@users.noreply.github.com>
2024-05-25 14:44:52 -0400

    Add test for constants

--- srp_test.go
+++ srp_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"crypto/sha1"
 	"encoding/csv"
 	"encoding/hex"
+	"math/big"
 	"os"
 	"testing"
 
@@ -34,6 +36,27 @@ func loadTestData(path string) [][]string {
 	}
 
 	return rows
+}
+
+func Test_constants(t *testing.T) {
+	assert.Equal(t, bigK(), big.NewInt(3))
+	assert.Equal(t, bigG(), big.NewInt(7))
+	assert.Equal(t, bigN().Bytes(), []byte{
+		0x89, 0x4B, 0x64, 0x5E, 0x89, 0xE1, 0x53, 0x5B,
+		0xBD, 0xAD, 0x5B, 0x8B, 0x29, 0x06, 0x50, 0x53,
+		0x08, 0x01, 0xB1, 0x8E, 0xBF, 0xBF, 0x5E, 0x8F,
+		0xAB, 0x3C, 0x82, 0x87, 0x2A, 0x3E, 0x9B, 0xB7,
+	})
+
+	hBigN := sha1.Sum(largeSafePrime.LittleEndian().Bytes())
+	hBigG := sha1.Sum(bigG().Bytes())
+	computedXorHash := make([]byte, 20)
+
+	for i := 0; i < 20; i++ {
+		computedXorHash[i] = hBigN[i] ^ hBigG[i]
+	}
+
+	assert.Equal(t, xorHash.Bytes(), computedXorHash)
 }
 
 func Test_calcX(t *testing.T) {
