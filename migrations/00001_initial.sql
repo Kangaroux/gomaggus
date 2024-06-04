cf21349156a0c35a8d454c62b53a8184869f831a
Jessie <2302541+Kangaroux@users.noreply.github.com>
2024-06-04 15:35:31 -0400

    Add unique indexes to tables

--- migrations/00001_initial.sql
+++ migrations/00001_initial.sql
@@ -8,6 +8,9 @@ CREATE TABLE IF NOT EXISTS realms (
     host            varchar(100) NOT NULL,
     region          integer NOT NULL
 );
+CREATE UNIQUE INDEX IF NOT EXISTS realms_host_unique_idx ON realms (lower(host));
+CREATE UNIQUE INDEX IF NOT EXISTS realms_name_unique_idx ON realms (lower(name));
+
 CREATE TABLE IF NOT EXISTS accounts (
     id              serial PRIMARY KEY,
     created_at      timestamp NOT NULL DEFAULT now(),
@@ -16,17 +19,20 @@ CREATE TABLE IF NOT EXISTS accounts (
     srp_verifier    varchar(64) NOT NULL, -- 32 byte hex string
     srp_salt        varchar(64) NOT NULL, -- 32 byte hex string
     email           varchar(100) NOT NULL,
-    realm_id        integer NOT NULL REFERENCES realms(id)
+    realm_id        integer NOT NULL REFERENCES realms ON DELETE CASCADE
 );
+CREATE UNIQUE INDEX IF NOT EXISTS accounts_email_unique_idx ON accounts (lower(email));
+CREATE UNIQUE INDEX IF NOT EXISTS accounts_username_unique_idx ON accounts (upper(username));
+
 CREATE TABLE IF NOT EXISTS sessions (
     id              serial PRIMARY KEY,
-    connected       integer NOT NULL DEFAULT 0,
+    account         integer UNIQUE NOT NULL REFERENCES accounts ON DELETE CASCADE,
     session_key     varchar(80) NOT NULL, -- 40 byte hex string
+    connected       integer NOT NULL,
     connected_at    timestamp,
     disconnected_at timestamp
 );
 -- +goose StatementEnd
-
 
 -- +goose Down
 -- +goose StatementBegin
