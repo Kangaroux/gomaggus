f52568981d499002ca081bc96c47d157c0501a78
Jessie <2302541+Kangaroux@users.noreply.github.com>
2024-06-05 17:34:12 -0400

    Changing an account password now always generates a new salt

--- internal/models/account.go
+++ internal/models/account.go
@@ -25,22 +25,13 @@ type Account struct {
 }
 
 func (acc *Account) SetUsernamePassword(username, password string) error {
-	var err error
-	var salt []byte
-
-	if acc.SrpSaltHex == "" {
-		salt = make([]byte, 32)
-		if _, err = rand.Read(salt); err != nil {
-			return err
-		}
-		acc.SrpSaltHex = hex.EncodeToString(salt)
-	} else {
-		if salt, err = hex.DecodeString(acc.SrpSaltHex); err != nil {
-			return err
-		}
+	salt := make([]byte, srp.SaltSize)
+	if _, err := rand.Read(salt); err != nil {
+		return err
 	}
 
 	acc.Username = username
+	acc.SrpSaltHex = hex.EncodeToString(salt)
 	acc.SrpVerifierHex = hex.EncodeToString(srp.CalculateVerifier(username, password, salt))
 
 	return nil
