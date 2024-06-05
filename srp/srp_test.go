96437321a0bf2d57a451d1c86dabed58a4904fe9
Jessie <2302541+Kangaroux@users.noreply.github.com>
2024-06-05 15:33:27 -0400

    Add a couple test cases, reuse generated test data

--- srp/srp_test.go
+++ srp/srp_test.go
@@ -18,47 +18,55 @@ func decodeHex(s string) []byte {
 }
 
 func TestX(t *testing.T) {
-	t.Run("generated test data", func(t *testing.T) {
-		rows := internal.LoadTestData("../test_data/srp/calculate_x.csv")
+	rows := internal.LoadTestData("../test_data/srp/calculate_x.csv")
 
+	t.Run("generated test data", func(t *testing.T) {
 		for _, row := range rows {
 			username := row[0]
 			password := row[1]
 			salt := decodeHex(row[2])
 			expected := decodeHex(row[3])
+
 			assert.Equal(t, expected, CalculateX(username, password, salt))
 		}
 	})
 
-	t.Run("user/pass are case insensitive", func(t *testing.T) {
-		username := "test"
-		password := "password"
-		salt := decodeHex("84FD248366EBF8F258B632142B1F3588E7C49BA88D7CDF55753275E9607828B8")
-		first := CalculateX(username, password, salt)
+	t.Run("username/pass are case insensitive", func(t *testing.T) {
+		row := rows[0]
+		username := row[0]
+		password := row[1]
+		salt := decodeHex(row[2])
+
+		first := CalculateX(strings.ToLower(username), strings.ToLower(password), salt)
 		second := CalculateX(strings.ToUpper(username), strings.ToUpper(password), salt)
+
 		assert.Equal(t, first, second)
 	})
 }
 
 func TestVerifier(t *testing.T) {
-	t.Run("generated test data", func(t *testing.T) {
-		rows := internal.LoadTestData("../test_data/srp/calculate_verifier.csv")
+	rows := internal.LoadTestData("../test_data/srp/calculate_verifier.csv")
 
+	t.Run("generated test data", func(t *testing.T) {
 		for _, row := range rows {
 			username := row[0]
 			password := row[1]
 			salt := decodeHex(row[2])
 			expected := decodeHex(row[3])
+
 			assert.Equal(t, expected, CalculateVerifier(username, password, salt))
 		}
 	})
 
-	t.Run("user/pass are case insensitive", func(t *testing.T) {
-		username := "test"
-		password := "password"
-		salt := decodeHex("84FD248366EBF8F258B632142B1F3588E7C49BA88D7CDF55753275E9607828B8")
-		first := CalculateVerifier(username, password, salt)
+	t.Run("username/pass are case insensitive", func(t *testing.T) {
+		row := rows[0]
+		username := row[0]
+		password := row[1]
+		salt := decodeHex(row[2])
+
+		first := CalculateVerifier(strings.ToLower(username), strings.ToLower(password), salt)
 		second := CalculateVerifier(strings.ToUpper(username), strings.ToUpper(password), salt)
+
 		assert.Equal(t, first, second)
 	})
 }
@@ -70,6 +78,7 @@ func TestServerPublicKey(t *testing.T) {
 		verifier := decodeHex(row[0])
 		privateKey := decodeHex(row[1])
 		expected := decodeHex(row[2])
+
 		assert.Equal(t, expected, CalculateServerPublicKey(verifier, privateKey))
 	}
 }
@@ -81,6 +90,7 @@ func TestCalculateU(t *testing.T) {
 		clientPublic := decodeHex(row[0])
 		serverPublic := decodeHex(row[1])
 		expected := decodeHex(row[2])
+
 		assert.Equal(t, expected, CalculateU(clientPublic, serverPublic))
 	}
 }
@@ -94,6 +104,7 @@ func TestServerSKey(t *testing.T) {
 		u := decodeHex(row[2])
 		serverPrivate := decodeHex(row[3])
 		expected := decodeHex(row[4])
+
 		assert.Equal(t, expected, CalculateServerSKey(clientPublic, verifier, u, serverPrivate))
 	}
 }
@@ -104,6 +115,7 @@ func TestInterleave(t *testing.T) {
 	for _, row := range rows {
 		s := decodeHex(row[0])
 		expected := decodeHex(row[1])
+
 		assert.Equal(t, expected, CalculateInterleave(s))
 	}
 }
@@ -117,6 +129,7 @@ func TestServerSessionKey(t *testing.T) {
 		verifier := decodeHex(row[2])
 		expected := decodeHex(row[3])
 		serverPublic := CalculateServerPublicKey(verifier, serverPrivate)
+
 		assert.Equal(t, expected, CalculateServerSessionKey(clientPublic, serverPublic, serverPrivate, verifier))
 	}
 }
@@ -124,15 +137,32 @@ func TestServerSessionKey(t *testing.T) {
 func TestClientProof(t *testing.T) {
 	rows := internal.LoadTestData("../test_data/srp/calculate_client_proof.csv")
 
-	for _, row := range rows {
+	t.Run("generated test data", func(t *testing.T) {
+		for _, row := range rows {
+			username := row[0]
+			salt := decodeHex(row[1])
+			clientPublic := decodeHex(row[2])
+			serverPublic := decodeHex(row[3])
+			sessionKey := decodeHex(row[4])
+			expected := decodeHex(row[5])
+
+			assert.Equal(t, expected, CalculateClientProof(username, salt, clientPublic, serverPublic, sessionKey))
+		}
+	})
+
+	t.Run("username is case insensitive", func(t *testing.T) {
+		row := rows[0]
 		username := row[0]
 		salt := decodeHex(row[1])
 		clientPublic := decodeHex(row[2])
 		serverPublic := decodeHex(row[3])
 		sessionKey := decodeHex(row[4])
-		expected := decodeHex(row[5])
-		assert.Equal(t, expected, CalculateClientProof(username, salt, clientPublic, serverPublic, sessionKey))
-	}
+
+		first := CalculateClientProof(strings.ToLower(username), salt, clientPublic, serverPublic, sessionKey)
+		second := CalculateClientProof(strings.ToUpper(username), salt, clientPublic, serverPublic, sessionKey)
+
+		assert.Equal(t, first, second)
+	})
 }
 
 func TestServerProof(t *testing.T) {
@@ -143,6 +173,7 @@ func TestServerProof(t *testing.T) {
 		clientProof := decodeHex(row[1])
 		sessionKey := decodeHex(row[2])
 		expected := decodeHex(row[3])
+
 		assert.Equal(t, expected, CalculateServerProof(clientPublic, clientProof, sessionKey))
 	}
 }
@@ -150,12 +181,28 @@ func TestServerProof(t *testing.T) {
 func TestReconnectProof(t *testing.T) {
 	rows := internal.LoadTestData("../test_data/srp/calculate_reconnect_proof.csv")
 
-	for _, row := range rows {
+	t.Run("generated test data", func(t *testing.T) {
+		for _, row := range rows {
+			username := row[0]
+			clientData := decodeHex(row[1])
+			serverData := decodeHex(row[2])
+			sessionKey := decodeHex(row[3])
+			expected := decodeHex(row[4])
+
+			assert.Equal(t, expected, CalculateReconnectProof(username, clientData, serverData, sessionKey))
+		}
+	})
+
+	t.Run("username is case insensitive", func(t *testing.T) {
+		row := rows[0]
 		username := row[0]
 		clientData := decodeHex(row[1])
 		serverData := decodeHex(row[2])
 		sessionKey := decodeHex(row[3])
-		expected := decodeHex(row[4])
-		assert.Equal(t, expected, CalculateReconnectProof(username, clientData, serverData, sessionKey))
-	}
+
+		first := CalculateReconnectProof(strings.ToLower(username), clientData, serverData, sessionKey)
+		second := CalculateReconnectProof(strings.ToUpper(username), clientData, serverData, sessionKey)
+
+		assert.Equal(t, first, second)
+	})
 }
