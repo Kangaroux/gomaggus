72795dc5d86617677b794f3d939c4e0cc3c29ccb
Jessie <2302541+Kangaroux@users.noreply.github.com>
2024-06-14 17:22:05 -0400

    Add response structs for proof, add comments

--- realmd/handler/auth/proof.go
+++ realmd/handler/auth/proof.go
@@ -10,13 +10,29 @@ import (
 	"github.com/kangaroux/gomaggus/model"
 	"github.com/kangaroux/gomaggus/realmd"
 	"github.com/kangaroux/gomaggus/srp"
+	"github.com/mixcode/binarystruct"
+)
+
+// https://gtker.com/wow_messages/docs/billingplanflags.html
+type BillingFlag uint8
+
+const (
+	BillingNone          BillingFlag = 0x00
+	BillingUnused        BillingFlag = 0x01
+	BillingRecurringBill BillingFlag = 0x02
+	BillingFreeTrial     BillingFlag = 0x04
+	BillingIgr           BillingFlag = 0x08
+	BillingUsage         BillingFlag = 0x10
+	BillingTimeMixture   BillingFlag = 0x20
+	BillingRestricted    BillingFlag = 0x40
+	BillingEnableCais    BillingFlag = 0x80
 )
 
 // https://gtker.com/wow_messages/docs/cmsg_auth_session.html#client-version-335
-type authRequest struct {
+type proofRequest struct {
 	ClientBuild     uint32
 	LoginServerId   uint32
-	Username        string
+	Username        string `binary:"zstring"`
 	LoginServerType uint32
 	ClientSeed      [4]byte
 	RegionId        uint32
@@ -27,14 +43,28 @@ type authRequest struct {
 	AddonInfo       []byte
 }
 
-func ProofHandler(services *realmd.Service, client *realmd.Client, data []byte) error {
+// https://gtker.com/wow_messages/docs/smsg_auth_response.html#client-version-335
+type proofSuccess struct {
+	ResponseCode  realmd.ResponseCode
+	BillingTime   uint32
+	BillingFlags  BillingFlag
+	BillingRested uint32
+	Expansion     realmd.Expansion
+}
+
+// Unused for now
+// type proofWaitQueue struct {
+// 	ResponseCode         realmd.ResponseCode
+// 	HasFreeCharMigration bool
+// }
+
+func ProofHandler(svc *realmd.Service, client *realmd.Client, data []byte) error {
 	log.Println("starting auth session")
 
 	var err error
 	r := bytes.NewReader(data[6:])
 
-	// https://gtker.com/wow_messages/docs/cmsg_auth_session.html#client-version-335
-	p := authRequest{}
+	p := proofRequest{}
 	if err = binary.Read(r, binary.LittleEndian, &p.ClientBuild); err != nil {
 		return err
 	}
@@ -71,7 +101,7 @@ func ProofHandler(services *realmd.Service, client *realmd.Client, data []byte) 
 	}
 	p.AddonInfo = addonInfoBuf.Bytes()
 
-	client.Authenticated, err = authenticateClient(services, client, &p)
+	client.Authenticated, err = authenticateClient(svc, client, &p)
 	if err != nil {
 		return err
 	}
@@ -81,23 +111,25 @@ func ProofHandler(services *realmd.Service, client *realmd.Client, data []byte) 
 		return errors.New("client could not be authenticated")
 	}
 
-	inner := bytes.Buffer{}
-	inner.WriteByte(byte(realmd.RespCodeAuthOk))
-	inner.Write([]byte{0, 0, 0, 0})              // billing time
-	inner.WriteByte(0x0)                         // billing flags
-	inner.Write([]byte{0, 0, 0, 0})              // billing rested
-	inner.WriteByte(byte(realmd.ExpansionWrath)) // exp
+	resp := proofSuccess{
+		ResponseCode:  realmd.RespCodeAuthOk,
+		BillingTime:   0,
+		BillingFlags:  BillingNone,
+		BillingRested: 0,
+		Expansion:     realmd.ExpansionWrath,
+	}
 
-	// https://gtker.com/wow_messages/docs/smsg_auth_response.html#client-version-335
-	resp := bytes.Buffer{}
-	respHeader, err := realmd.BuildHeader(realmd.OpServerAuthResponse, uint32(inner.Len()))
+	buf := bytes.Buffer{}
+	if _, err := binarystruct.Write(&buf, binarystruct.LittleEndian, &resp); err != nil {
+		return err
+	}
+
+	header, err := client.BuildHeader(realmd.OpServerAuthResponse, uint32(buf.Len()))
 	if err != nil {
 		return err
 	}
-	resp.Write(client.Crypto.Encrypt(respHeader))
-	resp.Write(inner.Bytes())
 
-	if _, err := client.Conn.Write(resp.Bytes()); err != nil {
+	if _, err := client.Conn.Write(internal.ConcatBytes(header, buf.Bytes())); err != nil {
 		return err
 	}
 
@@ -106,28 +138,32 @@ func ProofHandler(services *realmd.Service, client *realmd.Client, data []byte) 
 	return nil
 }
 
-func authenticateClient(svc *realmd.Service, client *realmd.Client, p *authRequest) (bool, error) {
+// Returns whether the client is authenticated. Clients authenticate with authd and then send a proof
+// to realmd to show that they know the session key. We verify the proof by fetching the session key
+// from the DB, calculating the proof ourselves, and checking that it matches. If it does, the client
+// is considered authenticated.
+func authenticateClient(svc *realmd.Service, client *realmd.Client, p *proofRequest) (bool, error) {
 	var err error
 
 	client.Account, err = svc.Accounts.Get(&model.AccountGetParams{Username: p.Username})
 	if err != nil {
 		return false, err
 	} else if client.Account == nil {
-		log.Printf("no account with username %s exists", p.Username)
+		log.Printf("authenticateClient: no account with username %s exists", p.Username)
 		return false, nil
 	}
 
 	if client.Realm, err = svc.Realms.Get(p.RealmId); err != nil {
 		return false, err
 	} else if client.Realm == nil {
-		log.Printf("no realm with id %d exists", p.RealmId)
+		log.Printf("authenticateClient: no realm with id %d exists", p.RealmId)
 		return false, nil
 	}
 
 	if client.Session, err = svc.Sessions.Get(client.Account.Id); err != nil {
 		return false, err
 	} else if client.Session == nil {
-		log.Printf("no session for username %s exists", client.Account.Username)
+		log.Printf("authenticateClient: no session for username %s exists", client.Account.Username)
 		return false, nil
 	}
 
@@ -135,6 +171,7 @@ func authenticateClient(svc *realmd.Service, client *realmd.Client, p *authReque
 		return false, err
 	}
 
+	// Initialize the header encryption using the session key
 	client.Crypto = realmd.NewHeaderCrypto(client.Session.SessionKey())
 	if err := client.Crypto.Init(); err != nil {
 		return false, err
@@ -142,10 +179,9 @@ func authenticateClient(svc *realmd.Service, client *realmd.Client, p *authReque
 
 	proof := srp.CalculateWorldProof(p.Username, p.ClientSeed[:], client.ServerSeed, client.Session.SessionKey())
 
+	// Check to see if the proof sent by the client is correct
 	if !bytes.Equal(proof, p.ClientProof[:]) {
-		log.Println("proofs don't match")
-		log.Printf("got:    %x\n", p.ClientProof)
-		log.Printf("wanted: %x\n", proof)
+		log.Println("authenticateClient: invalid proof")
 		return false, nil
 	}
 
