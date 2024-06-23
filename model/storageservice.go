e82e266258311ff87b5283a2b0c4122c962aef65
Jessie <2302541+Kangaroux@users.noreply.github.com>
2024-06-22 21:00:52 -0400

    Add Get method to storage service

--- model/storageservice.go
+++ model/storageservice.go
@@ -7,6 +7,9 @@ import (
 )
 
 type AccountStorageService interface {
+	// Get returns the account storage with the given type. If it doesn't exist, the returned storag is nil.
+	Get(uint32, AccountStorageType) (*AccountStorage, error)
+
 	// List returns all storage belonging to accountID whose type matches mask. List may return fewer
 	// results than specified in the mask. The results are not ordered.
 	List(uint32, uint8) ([]*AccountStorage, error)
@@ -25,6 +28,18 @@ var _ AccountStorageService = (*DbAccountStorageService)(nil)
 
 func NewDbAccountStorageService(db *sqlx.DB) AccountStorageService {
 	return &DbAccountStorageService{db}
+}
+
+func (s *DbAccountStorageService) Get(accountID uint32, storageType AccountStorageType) (*AccountStorage, error) {
+	result := &AccountStorage{}
+	q := `SELECT * FROM account_storage WHERE account_id = $1 AND type = $2`
+	if err := s.db.Get(result, q, accountID, storageType); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return result, nil
 }
 
 func (s *DbAccountStorageService) List(accountID uint32, mask uint8) ([]*AccountStorage, error) {
@@ -118,6 +133,9 @@ func (s *DbAccountStorageService) UpdateOrCreate(storage *AccountStorage) (bool,
 }
 
 type CharacterStorageService interface {
+	// Get returns the character storage with the given type. If it doesn't exist, the returned storage is nil.
+	Get(uint32, CharacterStorageType) (*CharacterStorage, error)
+
 	// List returns all storage belonging to characterID whose type matches mask. List may return fewer
 	// results than specified in the mask. The results are not ordered.
 	List(uint32, uint8) ([]*CharacterStorage, error)
@@ -136,6 +154,18 @@ var _ CharacterStorageService = (*DbCharacterStorageService)(nil)
 
 func NewDbCharacterStorageService(db *sqlx.DB) CharacterStorageService {
 	return &DbCharacterStorageService{db}
+}
+
+func (s *DbCharacterStorageService) Get(characterID uint32, storageType CharacterStorageType) (*CharacterStorage, error) {
+	result := &CharacterStorage{}
+	q := `SELECT * FROM character_storage WHERE character_id = $1 AND type = $2`
+	if err := s.db.Get(result, q, characterID, storageType); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return result, nil
 }
 
 func (s *DbCharacterStorageService) List(characterID uint32, mask uint8) ([]*CharacterStorage, error) {
